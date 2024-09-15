4214f3f928a0fcee4e5c7293d543d3a7fad69437
Vladimir Zavediaev <vladimir@ennabl.com>
2024-09-15 17:04:36 +0300

    attempt 3

--- src/main/kotlin/com/zavediaev/Main.kt
+++ src/main/kotlin/com/zavediaev/Main.kt
@@ -4,5 +4,6 @@ fun main(args: Array<String>) {
     val path = args.firstOrNull() ?: "/Users/zavediaev-work/workspace/1brc/measurements.txt"
 
 //    attempt1(path) // 3:50.75
-    attempt2(path) // 2:38.56
+//    attempt2(path) // 2:38.56
+    attempt3(path) // 42.968
 }
