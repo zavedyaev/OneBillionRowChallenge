7b7a7014e0db376a389453cf71747bfe00304879
Vladimir Zavediaev <vladimir@ennabl.com>
2024-10-06 20:50:40 +0300

    cosmetic Main.kt changes

--- src/main/kotlin/com/zavediaev/obrc/Main.kt
+++ src/main/kotlin/com/zavediaev/obrc/Main.kt
@@ -1,18 +1,15 @@
 package com.zavediaev.obrc
 
+import com.zavediaev.obrc.attempt1.calc as attempt1
+import com.zavediaev.obrc.attempt2.calc as attempt2
+import com.zavediaev.obrc.attempt3.calc as attempt3
 import com.zavediaev.obrc.attempt4.calc as attempt4
-
-
-/**
- * compiled to jar using ./gradlew shadowJar
- * to start:
- * time java -cp OneBillionRowChallengeKotlin-1.0-SNAPSHOT-all.jar com.zavediaev.MainKt
- */
+import com.zavediaev.obrc.attempt5.calc as attempt5
+import com.zavediaev.obrc.attempt6.calc as attempt6
 
 fun main(args: Array<String>) {
     val path = args.firstOrNull() ?: "/Users/zavediaev-work/workspace/1brc/measurements.txt"
-
-    // time when running java -cp
+    // fastest file read: ~1.5 sec
 
 //    attempt1(path)
 //    attempt2(path)
@@ -20,7 +17,4 @@ fun main(args: Array<String>) {
     attempt4(path) // best
 //    attempt5(path)
 //    attempt6(path)
-
-
-    // fastest file read: 1.5 sec - so processing takes ~ 4 sec
 }
